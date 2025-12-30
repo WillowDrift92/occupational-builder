3ffb44480d3ad40fd56f4969387ea8a614ec8781
markbrada <marcotallarini@icloud.com>
2025-12-30 15:23:18 +1100

    Add minimal Vite React TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Vite + React + TypeScript</h1>
+      <p>Get started by editing src/App.tsx</p>
+    </div>
+  );
+}
+
+export default App;
